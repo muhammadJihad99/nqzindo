5de8a564699b495ed7164ead45c83a36024a359d
Muhammad Jihad <muhammadjihad093@gmail.com>
2026-05-14 01:31:37 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -92,3 +92,94 @@ contactForm.addEventListener("submit", (e) => {
 
   alert("Your message has been successfully sent!");
 });
+
+// dropdown
+const dropdown = document.querySelector(".dropdown");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+dropdown.addEventListener("click", (e) => {
+  if (window.innerWidth <= 900) {
+    e.preventDefault();
+    dropdownMenu.classList.toggle("active");
+  }
+});
+
+const oldCards = document.querySelectorAll(".oldteam-card");
+const lightbox = document.getElementById("oldteamLightbox");
+const lightboxImg = document.getElementById("lightboxImg");
+const lightboxName = document.getElementById("lightboxName");
+const lightboxRole = document.getElementById("lightboxRole");
+
+const closeLightbox = document.getElementById("closeLightbox");
+const oldPrevBtn = document.getElementById("oldPrevBtn");
+const oldNextBtn = document.getElementById("oldNextBtn");
+
+let oldIndex = 0;
+
+const oldMembers = [];
+
+oldCards.forEach((card, index) => {
+  const img = card.querySelector("img").src;
+  const name = card.querySelector("h3").innerText;
+  const role = card.querySelector("span").innerText;
+
+  oldMembers.push({ img, name, role });
+
+  card.addEventListener("click", () => {
+    oldIndex = index;
+    openLightbox(oldIndex);
+  });
+});
+
+function openLightbox(index) {
+  lightbox.classList.add("show");
+  updateLightbox(index);
+  document.body.style.overflow = "hidden";
+}
+
+function updateLightbox(index) {
+  lightboxImg.src = oldMembers[index].img;
+  lightboxName.innerText = oldMembers[index].name;
+  lightboxRole.innerText = oldMembers[index].role;
+}
+
+// CLOSE
+closeLightbox.addEventListener("click", () => {
+  lightbox.classList.remove("show");
+  document.body.style.overflow = "auto";
+});
+
+// NEXT
+oldNextBtn.addEventListener("click", () => {
+  oldIndex++;
+  if (oldIndex >= oldMembers.length) oldIndex = 0;
+  updateLightbox(oldIndex);
+});
+
+// PREV
+oldPrevBtn.addEventListener("click", () => {
+  oldIndex--;
+  if (oldIndex < 0) oldIndex = oldMembers.length - 1;
+  updateLightbox(oldIndex);
+});
+
+// CLICK OUTSIDE
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) {
+    lightbox.classList.remove("show");
+    document.body.style.overflow = "auto";
+  }
+});
+
+// KEYBOARD SUPPORT
+document.addEventListener("keydown", (e) => {
+  if (!lightbox.classList.contains("show")) return;
+
+  if (e.key === "Escape") {
+    lightbox.classList.remove("show");
+    document.body.style.overflow = "auto";
+  }
+
+  if (e.key === "ArrowRight") oldNextBtn.click();
+  if (e.key === "ArrowLeft") oldPrevBtn.click();
+});
